d06167abe10260e90cbbdf54b787b1a1d91bd44e
Rodrigo Fuenzalida <rfuenzalidac87@gmail.com>
2016-05-06 01:00:36 -0300

    test and add the ability to pick more procs

--- lucky.go
+++ lucky.go
@@ -3,8 +3,8 @@ package lucky
 import (
 	"errors"
 	"flag"
-	"fmt"
 	"log"
+	"runtime"
 	"time"
 
 	"github.com/rodrwan/lucky/db"
@@ -19,73 +19,69 @@ var (
 
 // Lucky ...
 type Lucky struct {
-	Model            map[string]*model.Samples
-	CatNum           map[uint]float64
+	Model            map[string]*model.Sample
 	CatStr           map[uint]string
 	LabelsPath       string
 	TrainingDataPath string
-	verbose          bool
+	Verbose          bool
 }
 
 // Fit ...
 func (newModel *Lucky) Fit() {
+	maxProcs := runtime.NumCPU()
+	log.Printf("Available CPU: %d\n", maxProcs)
+	runtime.GOMAXPROCS(maxProcs)
+
 	flag.Parse()
-	if !newModel.verbose {
-		newModel.verbose = *verb
+	if !newModel.Verbose {
+		newModel.Verbose = *verb
 	}
-	if newModel.verbose {
-		fmt.Println(">> Init model.")
+	if newModel.Verbose {
+		log.Println(">> Init model.")
 	}
 	if newModel.LabelsPath == "" {
 		newModel.LabelsPath = *labels
 	}
-	if newModel.verbose {
-		fmt.Printf("> Loading %s\n", newModel.LabelsPath)
+	if newModel.Verbose {
+		log.Printf("> Loading %s\n", newModel.LabelsPath)
 	}
 	if newModel.TrainingDataPath == "" {
 		newModel.TrainingDataPath = *path
 	}
-	if newModel.verbose {
-		fmt.Printf("> Loading %s\n", newModel.TrainingDataPath)
+	if newModel.Verbose {
+		log.Printf("> Loading %s\n", newModel.TrainingDataPath)
 	}
 
 	err := newModel.getCategories()
 	if err != nil {
 		log.Fatalln(err)
 	}
-	if newModel.verbose {
-		fmt.Println(">> Fit model.")
+	if newModel.Verbose {
+		log.Println(">> Fit model.")
 	}
 
 	start := time.Now()
-	m, c := model.Fit(newModel.TrainingDataPath)
+	m := model.Fit(newModel.TrainingDataPath, maxProcs)
 	elapsed := time.Since(start)
-	if newModel.verbose {
+	if newModel.Verbose {
 		log.Printf("Fit took %s\n\n", elapsed)
 	}
 
 	newModel.Model = m
-	newModel.CatNum = c
-	fmt.Println(">> Ready to categorize.")
+	log.Println(">> Ready to categorize.")
 }
 
 // Predict ...
 func (newModel *Lucky) Predict(test string) (res *model.BestCategory) {
 	m := newModel.Model
 	cats := newModel.CatStr
-	c := newModel.CatNum
-	res = model.Predict(m, test, cats, c)
+	res = model.Predict(m, test, cats)
 	return
 }
 
 func (newModel *Lucky) getCategories() error {
-	start := time.Now()
 	if model.Exists(newModel.LabelsPath) {
 		newModel.CatStr = db.Load(newModel.LabelsPath)
-		elapsed := time.Since(start)
-		if *verb {
-			log.Printf("Load labels took %s", elapsed)
-		}
 		return nil
 	}
 
--- lucky_test.go
+++ lucky_test.go
@@ -7,72 +7,92 @@ import (
 	"github.com/rodrwan/lucky/model"
 )
 
-func TestLucky(t *testing.T) {
-	fmt.Println("Lucky")
+func TestCatStr(t *testing.T) {
 	newModel := new(Lucky)
-	fmt.Printf("\t> CatNum")
-	if newModel.CatNum != nil {
-		fmt.Println(" Fail")
-		t.Errorf("CatNum should be nil, got %v", newModel.CatNum)
+	actual := newModel.CatStr
+	if actual != nil {
+		t.Errorf("CatStr should be nil, got %v", actual)
 	}
-	fmt.Println(" OK")
-	fmt.Printf("\t> CatStr")
-	if newModel.CatStr != nil {
-		fmt.Println(" Fail")
-		t.Errorf("CatStr should be nil, got %v", newModel.CatStr)
+}
+
+func TestLabelsPath(t *testing.T) {
+	newModel := new(Lucky)
+	expected := ""
+	actual := newModel.LabelsPath
+	if actual != expected {
+		t.Errorf("LabelsPath should be \"\", got %v", actual)
 	}
-	fmt.Println(" OK")
-	fmt.Printf("\t> LabelsPath")
-	if newModel.LabelsPath != "" {
-		fmt.Println(" Fail")
-		t.Errorf("LabelsPath should be \"\", got %v", newModel.LabelsPath)
+}
+func TestModel(t *testing.T) {
+	newModel := new(Lucky)
+	actual := newModel.Model
+	if actual != nil {
+		t.Errorf("Model should be nil, got %v", actual)
 	}
-	fmt.Println(" OK")
-	fmt.Printf("\t> Model")
-	if newModel.Model != nil {
-		fmt.Println(" Fail")
-		t.Errorf("Model should be nil, got %v", newModel.Model)
+}
+
+func TestTrainingDataPath(t *testing.T) {
+	newModel := new(Lucky)
+	expected := ""
+	actual := newModel.TrainingDataPath
+	if actual != expected {
+		t.Errorf("TrainingDataPath should be \"\", got %v", actual)
 	}
-	fmt.Println(" OK")
-	fmt.Printf("\t> TrainingDataPath")
-	if newModel.TrainingDataPath != "" {
-		fmt.Println(" Fail")
-		t.Errorf("TrainingDataPath should be \"\", got %v", newModel.TrainingDataPath)
+}
+
+func TestVerbose(t *testing.T) {
+	newModel := new(Lucky)
+	expected := false
+	actual := newModel.Verbose
+	if !actual == expected {
+		t.Errorf("TrainingDataPath should be \"\", got %v", actual)
 	}
-	fmt.Println(" OK")
-	fmt.Println()
 }
 
 func TestClassifier(t *testing.T) {
-	fmt.Println("Lucky classifier")
 	newModel := new(Lucky)
+	newModel.Verbose = true
 	newModel.Fit()
 	desc := "ADIDAS PARQUE ARAUCO"
 	best := newModel.Predict(desc)
-	// fmt.Println(best)
-	fmt.Printf("\t> Predict:")
 	if best.ID != 13 {
-		fmt.Println(" Fail")
 		t.Errorf(" Should return ID equal to 13, got %d", best.ID)
 	}
-	fmt.Println(" OK")
-	fmt.Printf("\t> Model:")
 	if newModel.Model == nil {
-		fmt.Println(" Fail")
 		t.Errorf(" Should not be equal to nil, got %v", newModel.Model)
 	}
-	fmt.Println(" OK")
-	fmt.Printf("\t> Model.bin:")
 	if !model.Exists("model.bin") {
-		fmt.Println(" Fail")
 		t.Errorf(" Should create model.bin, got %v", model.Exists("model.bin"))
 	}
-	fmt.Println(" OK")
-	fmt.Printf("\t> categories.bin:")
-	if !model.Exists("categories.bin") {
-		fmt.Println(" Fail")
-		t.Errorf("Should create categires.bin, got %v", model.Exists("categories.bin"))
-	}
-	fmt.Println(" OK")
 	fmt.Println()
+
+	var stringCase = []string{
+		"compra normal ripley parque arauco",
+		"compra normal paris parque arauco",
+		"compra normal falabella alto las condes",
+		"COMPRA SUBWAY METRO ESC",
+		"Giro en Cajero Automático",
+		"COMPRA NIKE SHOP ALTO LA",
+		"COMPRA LIQUIDOS LA REINA",
+		"COMPRA CASTANO LAS CONDE",
+		"COMPRA DONER HOUSE",
+		"PAC Seguro Frau 000005500832946",
+		"COMPRA PIZZA PIZZA COLON",
+		"COMPRA OK MARKET SAN PAS",
+		"COMPRA OK MARKET SUB CEN",
+		"COMPRA VIVE SNACK MOVIST",
+		"COMPRA BARBAZUL",
+		"COMPRA CASTANO LAS CONDE",
+		"COMPRA FUENTE CHILENA AP",
+		"PAGO EN LINEA VTR",
+		"PAGO EN LINEA AUTOPISTA COSTANERA NORTE",
+		"compra normal todo juegos cl",
+	}
+
+	for _, desc := range stringCase {
+		best := newModel.Predict(desc)
+		fmt.Printf("Description: %s\n", desc)
+		fmt.Printf("Category: %d -> %s\n", best.ID, best.Name)
+		fmt.Println()
+	}
 }
